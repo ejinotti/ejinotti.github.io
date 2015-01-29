c50161532b9e0079964336b336d5e30227ce7dbf
Edward J. Jinotti <ejinotti2@gmail.com>
2015-01-28 21:07:53 -0500

    Adds menu selection functionality.

---
+++ javascript.js
@@ -0,0 +1,23 @@
+function ZedUI () {
+  this.$currentTab = $("#menu li").filter(function () {
+    return $(this).text() === "about";
+  }).addClass("active");
+  $("#about").slideDown();
+  $("#menu").on("click", "li", this.selectTab.bind(this));
+}
+
+ZedUI.prototype.selectTab = function (event) {
+  var $newTab = $(event.currentTarget);
+
+  if ($newTab.text() === this.$currentTab.text()) return;
+
+  var $newSection = $("#" + $newTab.text());
+  var $currentSection = $("#" + this.$currentTab.text());
+
+  this.$currentTab.removeClass("active");
+  $newTab.addClass("active");
+
+  $currentSection.slideUp($.fn.slideDown.bind($newSection));
+
+  this.$currentTab = $newTab;
+};
